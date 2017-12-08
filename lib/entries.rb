87a8655d6742200cdd3b34eb86c5eb83a12715a4
Adrian Pastur <adrian.pastur@sage.com>
2017-12-08 18:00:11 +0000

    Added checksum calculation -

--- lib/entries.rb
+++ lib/entries.rb
@@ -10,19 +10,6 @@ class Entries
   end
 
   attr_reader :symbols, :account_number
-  attr_reader :d1, :d2, :d3, :d4, :d5, :d6, :d7, :d8, :d9
-
-  def position_names
-    @d1 = account_number[8].to_i
-    @d2 = account_number[7].to_i
-    @d3 = account_number[6].to_i
-    @d4 = account_number[5].to_i
-    @d5 = account_number[4].to_i
-    @d6 = account_number[3].to_i
-    @d7 = account_number[2].to_i
-    @d8 = account_number[1].to_i
-    @d9 = account_number[0].to_i
-  end
 
   def checksum_calculation
     d = 9
